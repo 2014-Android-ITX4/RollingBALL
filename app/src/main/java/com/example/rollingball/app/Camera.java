8624d9309ffa5aa260faac58a9894acc5a3d5663
Tukasa Kamata <kamatuka3@gmail.com>
2014-05-14 11:33:14 +0900

    fix #13 Camera.javaに追記

--- app/src/main/java/com/example/rollingball/app/Camera.java
+++ app/src/main/java/com/example/rollingball/app/Camera.java
@@ -1,7 +1,15 @@
 package com.example.rollingball.app;
 
+
+import com.hackoeur.jglm.Vec3;
+
 /**
  * Created by Tukasa on 2014/05/14.
  */
 public class Camera
-{}
+{
+  public Vec3 eye;
+  public Vec3 look_at;
+  public Vec3 up;
+  public float field_of_view;
+}
