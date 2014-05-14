a1399b53fd460c9f4b2f72e601810a283987ff80
walker <breveclown@gmail.com>
2014-05-14 10:54:15 +0900

    #6追加

---
+++ app/src/main/java/com/example/rollingball/app/MainView.java
@@ -0,0 +1,12 @@
+package com.example.rollingball.app;
+
+import android.opengl.GLSurfaceView;
+
+/**
+ * Created by sakamoto on 2014/05/14.
+ */
+public class MainView
+{
+  private MainRenderer _renderer;
+  private SceneManager _scene_manager;
+}
