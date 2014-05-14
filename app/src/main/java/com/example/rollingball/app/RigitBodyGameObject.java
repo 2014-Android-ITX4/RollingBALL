7ae980b5e73b65db2f5b4260e2af200ca932123d
walker <breveclown@gmail.com>
2014-05-14 13:48:45 +0900

    #14追加

---
+++ app/src/main/java/com/example/rollingball/app/RigitBodyGameObject.java
@@ -0,0 +1,14 @@
+package com.example.rollingball.app;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sakamoto on 2014/05/14.
+ */
+public class RigitBodyGameObject extends GameObject
+{
+  public Real mass;
+  public Vec3 velocity;
+  public ArrayList<Vec3> forces;
+
+}
