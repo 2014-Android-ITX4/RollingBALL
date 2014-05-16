abac1e56674d7e87950587e1d0c0e984b98ec600
Tukasa Kamata <kamatuka3@gmail.com>
2014-05-16 15:04:40 +0900

    #24 InputManager.javaの追加
    中身はこれから

---
+++ app/src/main/java/com/example/rollingball/app/InputManager.java
@@ -0,0 +1,49 @@
+package com.example.rollingball.app;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.view.KeyEvent;
+import com.hackoeur.jglm.Vec3;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+/**
+ * Created by Tukasa on 2014/05/14.
+ */
+public class InputManager implements IUpdatetable
+{
+
+  private boolean _keyboard_mode = false;
+  public Vec3 rotation = new Vec3();
+  private Scene _scene;
+
+  InputManager(Scene arg_scene)
+  {
+    _scene = arg_scene;
+  }
+
+  @Override
+  public void update( final long delta_time_in_ns )
+  {
+    update_from_keyboard();
+    update_from_gyroscope();
+  }
+
+  private void update_from_keyboard()
+  {
+    // ジャイロ入力状態なら何もせず抜ける
+    if( _keyboard_mode == false)
+      return;
+  }
+
+  private void update_from_gyroscope()
+  {
+    // キーボード入力状態なら何もせず抜ける
+    if( _keyboard_mode )
+      return;
+  }
+
+
+}
