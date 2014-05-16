6dd6b2df24a090184d36f47dc02eecedc04ff479
walker <breveclown@gmail.com>
2014-05-16 16:01:03 +0900

    実装#33完了

--- app/src/main/java/com/example/rollingball/app/Scene.java
+++ app/src/main/java/com/example/rollingball/app/Scene.java
@@ -1,21 +1,52 @@
 package com.example.rollingball.app;
 
+import android.hardware.input.InputManager;
+
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+
 /**
  * Created by sakamoto on 2014/05/14.
  */
-public class Scene
+public class Scene implements IUpdatetable
 {
-  public GameObject[] game_objects;
+  public LinkedList<GameObject> game_objects;
   public Camera camera;
   public InputManager input_manager;
   public SceneManager scene_manager;
-  private Messege[] _massages;
+  private Message[] _massages;
+  public boolean to_exit;
 
-  void push(GameObject game_object){
+  Scene(){
+    camera = new Camera();
+    input_manager = new InputManager();
+  }
+
+  Scene( SceneManager s )
+  {
+    scene_manager = s;
+  }
+
+  void push( GameObject game_object ){
 
   }
 
-  void message(Message message){
+  void message( Message message ){
 
+  }
+
+  public SceneManager getScene_manager(){
+    return scene_manager;
+  }
+
+  public void update( long delta_time_in_ns ){
+    if ( to_exit == true )
+    {
+      for ( int i = 0; i < game_objects.size(); i++ )
+      {
+        update( delta_time_in_ns );
+      }
+      to_exit = false;
+    }
   }
 }
