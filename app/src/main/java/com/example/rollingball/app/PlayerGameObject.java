ff019e09446f85bc9b43aa497e4075ce152d7dbe
Usagi Ito <usagi@WonderRabbitProject.net>
2014-05-21 12:14:05 +0900

    fix #63 invincible スペルミス修正

--- app/src/main/java/com/example/rollingball/app/PlayerGameObject.java
+++ app/src/main/java/com/example/rollingball/app/PlayerGameObject.java
@@ -2,11 +2,8 @@ package com.example.rollingball.app;
 
 import java.util.LinkedList;
 
-/**
- * Created by sakamoto on 2014/05/14.
- */
 public class PlayerGameObject extends RigidBodyGameObject
 {
   public LinkedList< Item > items;
-  public double invinsible_time;
+  public double invincible_time;
 }
