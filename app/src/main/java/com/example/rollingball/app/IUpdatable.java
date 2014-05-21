ac7d69348aaae4c0b514bcac807cfc59d20f4b78
nabeshiho <toritobuheya@gmail.com>
2014-05-21 16:17:09 +0900

    #28 追加

---
+++ app/src/main/java/com/example/rollingball/app/IUpdatable.java
@@ -0,0 +1,9 @@
+package com.example.rollingball.app;
+
+/**
+ * Created by watanabeshiho on 2014/05/21.
+ */
+public interface IUpdatable
+{
+
+}
--- app/src/main/java/com/example/rollingball/app/SceneManager.java
+++ app/src/main/java/com/example/rollingball/app/SceneManager.java
@@ -1,17 +1,64 @@
 package com.example.rollingball.app;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
 /**
  * Created by sakamoto on 2014/05/14.
  */
-public class SceneManager
+public class SceneManager implements IUpdatable
 {
-  public Scene scenes;
+  private ArrayDeque< Scene > _scenes = new ArrayDeque< Scene >();
 
-  void push(Scene scene){
-
+  public static void main( String[] args ) throws java.lang.Exception
+  {
+    _scenes.addScene(  );
+    _scenes.addScene(  );
+    _scenes.addScene(  );
+    _scenes.addScene(  );
+    _scenes.addScene(  );
   }
 
-  void replace(Scene scene){
+  // MainView 参照コンストラクタ
+  private MainView view;
+  SceneManager( MainView view_ ){
+    view = view_;
+  }
 
+  void push( Scene scene )
+  {
+    _scenes.push( scene );
+    scene.scene_manager = this;
+  }
+
+  void replace( Scene scene )
+  {
+    _scenes.pop();
+    _scenes.push( scene );
+  }
+
+  void update( Scene scene )
+  {
+    scene = _scenes.peek();
+    update( scene );
+
+    //シーンの bool to_exit が true ならばそのシーンをpopする
+    boolean to_exit  = false;
+    if (scene == exit){
+      to_exit = true;
+    }
+    if (to_exit == true)
+    {
+      _scenes.pop();
+      update( scene );
+    }
+
+    if ( _scenes == null )
+    {
+      Scene TestScene = new TestScene;
+      _scenes.push( TestScene );
+      // TODO: version-0.2.0 レベルで作成される BlandLogoScene の自動push
+      _scenes.push(BlandLogoScene);
+    }
   }
 }
