0886be8f48ee3fd57a07052fcc914c2c20ee7230
Tukasa Kamata <kamatuka3@gmail.com>
2014-05-21 10:01:47 +0900

    fix #57 LifeGameObjectの修正

--- app/src/main/java/com/example/rollingball/app/RigidBodyGameObject.java
+++ app/src/main/java/com/example/rollingball/app/RigidBodyGameObject.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
 /**
  * Created by sakamoto on 2014/05/14.
  */
-public class RigidBodyGameObject
-  extends GameObject
+public class RigidBodyGameObject extends GameObject
 {
   public float mass;
   public Vec3 velocity;
