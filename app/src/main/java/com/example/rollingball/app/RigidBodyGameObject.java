bbca6473cf1023b472d67c79d09b14998b75b0b4
Tukasa Kamata <kamatuka3@gmail.com>
2014-05-21 09:58:53 +0900

    fix #55 RigidBodyGameObjectの修正
    PlayerGameObjectもクラス名変更により自動修正されました

--- app/src/main/java/com/example/rollingball/app/RigidBodyGameObject.java
+++ app/src/main/java/com/example/rollingball/app/RigidBodyGameObject.java
@@ -1,13 +1,15 @@
 package com.example.rollingball.app;
 
+import com.hackoeur.jglm.Vec3;
 import java.util.ArrayList;
 
 /**
  * Created by sakamoto on 2014/05/14.
  */
-public class RigitBodyGameObject extends GameObject
+public class RigidBodyGameObject
+  extends GameObject
 {
-  public Real mass;
+  public float mass;
   public Vec3 velocity;
   public ArrayList<Vec3> forces;
 
