66328b5f91a898bed5c35daf31ae351a23148294
aaaaaaai <arakawa85@gmail.com>
2014-05-28 14:04:48 +0900

    CameraクラスにIUpdatableを追加

--- app/src/main/java/com/example/rollingball/app/Camera.java
+++ app/src/main/java/com/example/rollingball/app/Camera.java
@@ -6,10 +6,16 @@ import com.hackoeur.jglm.Vec3;
 /**
  * Created by Tukasa on 2014/05/14.
  */
-public class Camera
+public class Camera implements IUpdatable
 {
   public Vec3 eye;
   public Vec3 look_at;
   public Vec3 up;
   public float field_of_view;
+
+  @Override
+  public void update( final long delta_time_in_ns )
+  {
+
+  }
 }
