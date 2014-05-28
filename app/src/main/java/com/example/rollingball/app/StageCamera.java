439c6cf18e2c0974d767091e2acbebddcda20344
aaaaaaai <arakawa85@gmail.com>
2014-05-28 14:10:45 +0900

    #48StageCamera追加・実�

---
+++ app/src/main/java/com/example/rollingball/app/StageCamera.java
@@ -0,0 +1,34 @@
+package com.example.rollingball.app;
+
+import android.opengl.GLU;
+
+import com.hackoeur.jglm.Vec3;
+
+/**
+ * Created by arakawa on 2014/05/21.
+ */
+public class StageCamera extends Camera
+{
+  public float distance;    //r
+  public float theta = 0;   //θ
+  public float phi = 30;    //φ
+
+  @Override
+  public void update( long delta_time_in_ns )
+  {
+    super.update( delta_time_in_ns );
+
+    float x = distance * ( float ) Math.sin( theta ) * ( float ) Math.cos( phi );
+    float y = distance * ( float ) Math.sin( theta ) * ( float ) Math.sin( phi );
+    float z = distance * ( float ) Math.cos( theta );
+
+    Vec3 a = new Vec3( x, y, z );
+
+    GameObject gameObject = new GameObject();
+    gameObject.position = look_at;
+
+    Camera camera = new Camera();
+    camera.eye = new Vec3( look_at.getX() + a.getX(), look_at.getY() + a.getY(), look_at.getZ() + a.getZ() );
+
+  }
+}
