d03c9aecf32ab71017a5079be7ba8350e3e7a6aa
nabeshiho <toritobuheya@gmail.com>
2014-05-30 16:10:59 +0900

    #82 途中まで
    色を付ける
    
    参考サイト：
    http://dev.classmethod.jp/etc/open-gl-es-2/
    http://junkcode.aakaka.com/archives/220

--- app/src/main/java/com/example/rollingball/app/TestScene.java
+++ app/src/main/java/com/example/rollingball/app/TestScene.java
@@ -1,46 +1,76 @@
 package com.example.rollingball.app;
 import android.opengl.GLES20;
+import android.opengl.GLSurfaceView.Renderer;
 import android.util.Log;
-
-import com.hackoeur.jglm.Vec;
 import com.hackoeur.jglm.Vec3;
-
+import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public class TestScene extends Scene
-{
-  private static final float[] Position = {0.5f, 0.5f, 0.0f};
-  float[] rotation_values = new float[3];
 
+public class TestScene extends Scene implements Renderer
+{
   TestScene( final SceneManager s )
   {
     super( s );
 
     // ログを出力
     Log.v( "tag", "message" );
+  }
 
-    // xyz --> rgb;色を割り当てて、色の変化で傾きをとれていることを確認できるようにする。
-    rotation_values = InputManager rotation;
-
-    // 描画処理
-    public void draw( GL10 gl ){
-
-    // 頂点配列を有効化します
-    gl.glEnableClientState( GL10.GL_VERTEX_ARRAY );
-    gl.glVertexPointer( 3, GL10.GL_FLOAT , 0, rotation_values);
-
-    gl.glNormal3f( 1.0f, 0.0f, 0.5f ); // x　赤
-
-    gl.glNormal3f( 0.0f, 0.0f, 1.0f ); // y　青
-
-    gl.glNormal3f( 1.0f, 1.0f, 0.0f ); // z　黄
-    }
-   }
+  // InputManagerから値を読み取る
+  public Vec3 test = input_manager.rotation;
+  float[] vertices  = test.getArray();
 
   @Override
   public void update( final long delta_time_in_ns )
   {
     super.update( delta_time_in_ns );
     GLES20.glClearColor( 0.9f, 0.9f, 1.0f, 1.0f ); // 初期化
+  }
+
+  @Override
+  public void onSurfaceCreated(
+    final GL10 gl10, final EGLConfig eglConfig
+  )
+  {
+
+  }
+
+  @Override
+  // 大きさが変更されたときに呼び出し
+  public void onSurfaceChanged( final GL10 gl, final int width, final int height )
+  {
+    //描画範囲の指定
+    gl.glViewport(0, 0, width, height);
+  }
+
+  @Override
+  public void onDrawFrame( final GL10 gl)
+  {
+    // xyz --> rgb;色を割り当てて、色の変化で傾きをとれていることを確認できるようにする。
+    int vertexIdx = 0;
+
+    // 原点
+    vertices[vertexIdx++] = 0.0f;       // x
+    vertices[vertexIdx++] = 0.0f;       // y
+    vertices[vertexIdx++] = 1.0f;       // z
+
+    // 右上
+    vertices[vertexIdx++] = 1.0f;
+    vertices[vertexIdx++] = 1.0f;
+    vertices[vertexIdx++] = 1.0f;
+
+    // 右下
+    vertices[vertexIdx++] = 1.0f;
+    vertices[vertexIdx++] = 0.0f;
+    vertices[vertexIdx++] = 1.0f;
+
+    // 頂点座標を有効にする
+    gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+    //頂点カラーを有効にする
+    GLES20.glEnableVertexAttribArray(this.mLocColor);
+    
+    //頂点3点を使って三角形を描画
+    gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
   }
 }
