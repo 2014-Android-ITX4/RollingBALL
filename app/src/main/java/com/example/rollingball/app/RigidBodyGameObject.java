2059488d623b6244c0b13861e381ba8cb97eecfe
walker <breveclown@gmail.com>
2014-05-30 13:40:34 +0900

    #74修正完了

--- app/src/main/java/com/example/rollingball/app/RigidBodyGameObject.java
+++ app/src/main/java/com/example/rollingball/app/RigidBodyGameObject.java
@@ -9,14 +9,15 @@ import java.util.ArrayList;
 public class RigidBodyGameObject extends GameObject
 {
   public float mass;                //質量
-  public Vec3 velocity,acceleration;//速度
-  public Vec3 force_sum;
+  public Vec3 velocity;//速度
   public ArrayList<Vec3> forces;    //力
 
   @Override
   public void update( final long delta_time_in_ns )
   {
     super.update( delta_time_in_ns );
+    Vec3 acceleration;
+    Vec3 force_sum = null;
 
     for( Vec3 v: forces ){
       force_sum = force_sum.add( v );
