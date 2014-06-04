2688c6765da0997ac1b2cd9ed846424b6ee6264c
nabeshiho <toritobuheya@gmail.com>
2014-06-04 12:58:04 +0900

    82

--- app/src/main/java/com/example/rollingball/app/TestScene.java
+++ app/src/main/java/com/example/rollingball/app/TestScene.java
@@ -1,11 +1,20 @@
 package com.example.rollingball.app;
+import java.nio.FloatBuffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView.Renderer;
 import android.util.Log;
 import com.hackoeur.jglm.Vec3;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
-
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView.BufferType;
+import android.graphics.Color;
 
 public class TestScene extends Scene implements Renderer
 {
@@ -15,11 +24,13 @@ public class TestScene extends Scene implements Renderer
 
     // ログを出力
     Log.v( "tag", "message" );
+
+   
   }
 
   // InputManagerから値を読み取る
   public Vec3 test = input_manager.rotation;
-  float[] vertices  = test.getArray();
+  float[] ver_i  = test.getArray();
 
   @Override
   public void update( final long delta_time_in_ns )
@@ -29,15 +40,13 @@ public class TestScene extends Scene implements Renderer
   }
 
   @Override
-  public void onSurfaceCreated(
-    final GL10 gl10, final EGLConfig eglConfig
-  )
+  public void onSurfaceCreated(final GL10 gl10, final EGLConfig eglConfig)
   {
 
   }
 
   @Override
-  // 大きさが変更されたときに呼び出し
+  // 変更されたときに呼び出し
   public void onSurfaceChanged( final GL10 gl, final int width, final int height )
   {
     //描画範囲の指定
@@ -50,26 +59,42 @@ public class TestScene extends Scene implements Renderer
     // xyz --> rgb;色を割り当てて、色の変化で傾きをとれていることを確認できるようにする。
     int vertexIdx = 0;
 
+    float[] vertices = new float[3*6];
+
     // 原点
-    vertices[vertexIdx++] = 0.0f;       // x
-    vertices[vertexIdx++] = 0.0f;       // y
-    vertices[vertexIdx++] = 1.0f;       // z
+    vertices[ vertexIdx++ ] = ver_i[ 0 ];
+    vertices[ vertexIdx++ ] = ver_i[ 1 ];
+    vertices[ vertexIdx++ ] = ver_i[ 2 ];
+    vertices[ vertexIdx++ ] = 0.0f;       // x
+    vertices[ vertexIdx++ ] = 0.0f;       // y
+    vertices[ vertexIdx++ ] = 1.0f;       // z
 
     // 右上
-    vertices[vertexIdx++] = 1.0f;
-    vertices[vertexIdx++] = 1.0f;
-    vertices[vertexIdx++] = 1.0f;
+    vertices[ vertexIdx++ ] = ver_i[ 3 ];
+    vertices[ vertexIdx++ ] = ver_i[ 4 ];
+    vertices[ vertexIdx++ ] = ver_i[ 5 ];
+    vertices[ vertexIdx++ ] = 1.0f;
+    vertices[ vertexIdx++ ] = 1.0f;
+    vertices[ vertexIdx++ ] = 1.0f;
 
     // 右下
-    vertices[vertexIdx++] = 1.0f;
-    vertices[vertexIdx++] = 0.0f;
-    vertices[vertexIdx++] = 1.0f;
+    vertices[ vertexIdx++ ] = ver_i[ 6 ];
+    vertices[ vertexIdx++ ] = ver_i[ 7 ];
+    vertices[ vertexIdx++ ] = ver_i[ 8 ];
+    vertices[ vertexIdx++ ] = 1.0f;
+    vertices[ vertexIdx++ ] = 0.0f;
+    vertices[ vertexIdx++ ] = 1.0f;
 
-    // 頂点座標を有効にする
-    gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-    //頂点カラーを有効にする
-    GLES20.glEnableVertexAttribArray(this.mLocColor);
-    
+    ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
+    bb.order(ByteOrder.nativeOrder());
+    // ログを出力
+    Log.v( "tag", "message" );
+
+    FloatBuffer fb = bb.asFloatBuffer();
+    fb.put(vertices);
+    //ログ出力
+    Log.v( "tag", "message" );
+
     //頂点3点を使って三角形を描画
     gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
   }
