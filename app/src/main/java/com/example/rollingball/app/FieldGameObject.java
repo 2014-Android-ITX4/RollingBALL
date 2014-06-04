afc5ad9485a4ab8f36c5d43080566d831a9396cf
walker <breveclown@gmail.com>
2014-06-04 13:30:02 +0900

    #106修正完了

--- app/src/main/java/com/example/rollingball/app/FieldGameObject.java
+++ app/src/main/java/com/example/rollingball/app/FieldGameObject.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class FieldGameObject extends GameObject
 {
   private ArrayList< ArrayList< Float > > field_planes = new ArrayList< ArrayList< Float > >();
-  private ArrayList< Float > field_lines = new ArrayList< Float >();
 
   public void generate_simple_plane( int arris_x, int arris_y)
   {
+    ArrayList< Float > field_lines = new ArrayList< Float >();
     for ( int x = 0 ; x < arris_x ; x++ ){
       for ( int y = 0 ; y < arris_y ; y++ ){
         if ( x == 0 || x == arris_x - 1 ) field_lines.add( 1.0f );
@@ -16,7 +16,7 @@ public class FieldGameObject extends GameObject
         else field_lines.add( 0.0f );
       }
       field_planes.add( field_lines );
-      field_lines.clear();
+      field_lines = new ArrayList< Float >();
     }
 
 
