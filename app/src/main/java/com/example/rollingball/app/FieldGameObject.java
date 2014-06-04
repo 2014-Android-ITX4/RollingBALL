e2d3d4b1ed339994350a916b2849c8343652403e
Usagi Ito <usagi@WonderRabbitProject.net>
2014-06-04 14:27:15 +0900

    reformat

--- app/src/main/java/com/example/rollingball/app/FieldGameObject.java
+++ app/src/main/java/com/example/rollingball/app/FieldGameObject.java
@@ -9,16 +9,24 @@ public class FieldGameObject extends GameObject
   public void generate_simple_plane( int arris_x, int arris_z)
   {
     ArrayList< Float > field_lines = new ArrayList< Float >();
-    for ( int x = 0 ; x < arris_x ; x++ ){
-      for ( int z = 0 ; z < arris_z ; z++ ){
-        if ( x == 0 || x == arris_x - 1 ) field_lines.add( 1.0f );
-        else if (z == 0 || z == arris_z - 1 ) field_lines.add( 1.0f );
-        else field_lines.add( 0.0f );
+
+    for ( int x = 0 ; x < arris_x ; x++ )
+    {
+      for ( int z = 0 ; z < arris_z ; z++ )
+      {
+        if ( x == 0 || x == arris_x - 1 )
+          field_lines.add( 1.0f );
+        else if (z == 0 || z == arris_z - 1 )
+          field_lines.add( 1.0f );
+        else
+          field_lines.add( 0.0f );
       }
       field_planes.add( field_lines );
       field_lines = new ArrayList< Float >();
     }
 
   }
-  public void load_from_file(){ throw new NotImplementedException(); }
+
+  public void load_from_file()
+  { throw new NotImplementedException(); }
 }
