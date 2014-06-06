778ac6a68fd313ae6ad35a5314aaf18ff7efab86
Tukasa Kamata <kamatuka3@gmail.com>
2014-06-06 15:07:27 +0900

    #39 UserDataManagerに追加
    SQL文を書き始めました

--- app/src/main/java/com/example/rollingball/app/UserDataManager.java
+++ app/src/main/java/com/example/rollingball/app/UserDataManager.java
@@ -1,7 +1,57 @@
 package com.example.rollingball.app;
 
-public class UserDataManager
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class UserDataManager extends SQLiteOpenHelper
 {
-  public void save(){}
+
+  UserDataManager( Context context )
+  {
+    // DBを作成します
+    super( context, "SAVEDATA",  null, 1 );
+  }
+
+  public void save_config( String name, float value)
+  {
+    // TODO:引数を元にDBのコンフィグの値を更新
+  }
+
+  public void save_stage( int world_id, int stage_id, int score, int rank )
+  {
+    // TODO:引数を元にステージのスコア・ランクの値を更新
+  }
+
+  public void save_item( int item_id, int number )
+  {
+    // TODO:引数を元にアイテムの値を更新
+  }
+
   public void load(){}
+
+
+  // データベースが作成された時に呼び出されます
+  @Override
+  public void onCreate( final SQLiteDatabase db )
+  {
+    // CONFIGテーブルを作成
+    db.execSQL( "create table CONFIG " +
+                  "(name text," +
+                  "value real," +
+                  "primary key(name))" );
+
+    db.execSQL( "create table STAGE" +
+                  "(world_id integer," +
+                  "stage_id integer," +
+                  "score integer" +
+                  "rank integer" +
+                  "primary key(world_id, stage_id)" );
+  }
+
+  @Override
+  public void onUpgrade( final SQLiteDatabase sqLiteDatabase, final int i, final int i2 )
+  {
+
+  }
 }
