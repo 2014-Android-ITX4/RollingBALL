70fab4e9cd48aad1a2369b5ab49b2f1754b5af99
Tukasa Kamata <kamatuka3@gmail.com>
2014-06-11 15:04:13 +0900

    #39 UserDataManagerに追加

--- app/src/main/java/com/example/rollingball/app/UserDataManager.java
+++ app/src/main/java/com/example/rollingball/app/UserDataManager.java
@@ -19,18 +19,28 @@ public class UserDataManager
 
     helper = new SaveDataSQLiteOpenHelper( manager.view.activity );
     db = helper.getWritableDatabase();
+
+    // CONFIGテーブルが空 = データ入ってなければ初期データ挿入
+    Cursor cursor = db.rawQuery( "select * from CONFIG", null );
+    cursor.moveToFirst();
+
+    if ( cursor.getCount() == 0 )
+      insert_newdata();
   }
 
   // CONFIGの設定情報を更新します
   public void save_config( String name, float value)
   {
-    String sql;
+    ContentValues values = new ContentValues();
+    values.put( "value", value );
 
-    sql = "update CONFIG set value = " + String.valueOf( value ) +
-          ", where name = '" + name + "'";
-    db.execSQL( sql );
-
-
+    try
+    {
+      db.update( "CONFIG", values, "name = ?", new String[]{ name } );
+    } catch ( SQLException e )
+    {
+      Log.e( "save_config SQL ERROR", e.toString() );
+    }
  }
 
   // 引数のステージデータをセーブします
@@ -40,8 +50,16 @@ public class UserDataManager
     values.put( "score", score );
     values.put( "rank", rank );
 
-    db.update( "STAGE", values, "world_id = ? AND stage_id = ?",
-               new String[]{ String.valueOf( world_id ), String.valueOf( stage_id ) } );
+    try
+    {
+      db.update( "STAGE", values, "world_id = ? AND stage_id = ?",
+                 new String[]{ String.valueOf( world_id ), String.valueOf( stage_id ) } );
+    } catch ( SQLException e )
+    {
+      Log.e( "save_stage SQL ERROR", e.toString() );
+    }
+
+
   }
 
   // アイテムの個数を更新します
@@ -50,25 +68,27 @@ public class UserDataManager
     ContentValues values = new ContentValues();
     values.put( "number", number );
 
-    db.update( "ITEM", values, "item_id = ?", new String[]{String.valueOf( item_id ) });
+    try
+    {
+      db.update( "ITEM", values, "item_id = ?", new String[]{String.valueOf( item_id ) });
+    } catch ( SQLException e )
+    {
+      Log.e( "save_item SQL ERROR", e.toString() );
+    }
 
-  }
 
-  public void load()
-  {
-    Cursor cursor;
   }
 
   // 引数を元に設定の値をfloatで返します
   public float load_config( String search_name )
   {
 
-    float value;
+    float value = -1;
     Cursor cursor;
 
     // 検索条件要変数
     String table = "CONFIG"; // テーブル名
-    String[] colums = { "value" }; // 引き出す項目
+    String[] columns = { "value" }; // 引き出す項目
     String selection = "name = ?"; // 検索条件
     String[] selection_args = { search_name }; //検索条件の"?"部分
     String group_by = null; // groupBy句
@@ -76,12 +96,20 @@ public class UserDataManager
     String order_by = null; // order_by句
 
     // SQL実行
-    cursor = db.query( table, colums, selection, selection_args,
-                       group_by, having, order_by);
-    cursor.moveToFirst();
+    try
+    {
+      cursor = db.query(
+        table, columns, selection, selection_args, group_by, having, order_by
+      );
+      cursor.moveToFirst();
 
-    // 値をvalueに格納
-    value = cursor.getFloat( cursor.getColumnIndex( colums[0] ) );
+      // 値をvalueに格納
+      value = cursor.getFloat( cursor.getColumnIndex( columns[0] ) );
+    } catch ( SQLException e )
+    {
+      Log.e( "load_config SQL ERROR", e.toString() );
+    }
+
 
     return value;
   }
@@ -89,13 +117,13 @@ public class UserDataManager
   // 引数を元にスコアとランクをintの配列で返します
   public int[] load_score( int world_id, int stage_id )
   {
-    int[] score_and_rank = { 0, 0 };
+    int[] score_and_rank = { -1, -1 };
 
     Cursor cursor;
 
     // 検索条件要変数
     String table = "STAGE"; // テーブル名
-    String[] colums = { "score", "rank" }; // 引き出す項目
+    String[] columns = { "score", "rank" }; // 引き出す項目
     String selection = "world_id = ? AND stage_id = ?"; // 検索条件
     String[] selection_args = { String.valueOf( world_id ), String.valueOf( stage_id ) }; //検索条件の"?"部分
     String group_by = null; // groupBy句
@@ -103,25 +131,33 @@ public class UserDataManager
     String order_by = null; // order_by句
 
     // SQL実行
-    cursor = db.query( table, colums, selection, selection_args,
-                       group_by, having, order_by);
-    cursor.moveToFirst();
+    try
+    {
+      cursor = db.query( table, columns, selection, selection_args,
+                         group_by, having, order_by);
+      cursor.moveToFirst();
 
-    // cursorから配列へスコアとランクを格納
-    score_and_rank[ 0 ] = cursor.getInt( cursor.getColumnIndex( "score" ) );
-    score_and_rank[ 1 ] = cursor.getInt( cursor.getColumnIndex( "rank" ) );
+      // cursorから配列へスコアとランクを格納
+      score_and_rank[ 0 ] = cursor.getInt( cursor.getColumnIndex( "score" ) );
+      score_and_rank[ 1 ] = cursor.getInt( cursor.getColumnIndex( "rank" ) );
+    } catch ( SQLException e )
+    {
+      Log.e( "load_score SQL ERROR", e.toString() );
+    }
+
 
     return score_and_rank;
   }
 
+  // アイテムの所持数を返します
   public int load_item(int item_id)
   {
-    int id;
+    int id = -1;
     Cursor cursor;
 
     // 検索条件要変数
     String table = "ITEM"; // テーブル名
-    String[] colums = { "number" }; // 引き出す項目
+    String[] columns = { "number" }; // 引き出す項目
     String selection = "item_id = ?"; // 検索条件
     String[] selection_args = { String.valueOf( item_id ) }; //検索条件の"?"部分
     String group_by = null; // groupBy句
@@ -129,11 +165,19 @@ public class UserDataManager
     String order_by = null; // order_by句
 
     // SQL実行
-    cursor = db.query( table, colums, selection, selection_args,
-                       group_by, having, order_by);
-    cursor.moveToFirst();
+    try
+    {
 
-    id = cursor.getInt( cursor.getColumnIndex( "number" ) );
+      cursor = db.query( table, columns, selection, selection_args,
+                         group_by, having, order_by);
+      cursor.moveToFirst();
+
+      id = cursor.getInt( cursor.getColumnIndex( "number" ) );
+    } catch ( SQLException e )
+    {
+      Log.e( "load_item SQL ERROR", e.toString() );
+    }
+
 
     return id;
   }
@@ -142,16 +186,50 @@ public class UserDataManager
   private void insert_newdata()
   {
 
+    ContentValues values = new ContentValues();
+
     // TODO:変数仮置き、あとで各項目の取得方法を決めておく
     String[] config_name = { "BGM", "SE" }; // 各設定項目の名前
+    float[] config_default = { 50, 50 };
     int[]  stage_value = { 4, 4, 4 }; // ワールドごとのステージ数
     int item_value = 30; // アイテム数
 
     try
     {
-      for ( String name : config_name )
+      // 設定分
+      for ( int i = 0; i < config_name.length; i++ )
       {
-        // TODO:SQL文書く
+        values.clear();
+
+        values.put( "name", config_name[i] );
+        values.put( "value", config_default[i] );
+
+        db.insert( "CONFIG", null, values );
+      }
+
+      // スコア分
+      for(int world = 0; world < config_default.length; world++)
+      {
+        for ( int stage = 0; stage < config_default[world]; stage++ )
+        {
+          values.clear();
+          values.put( "world_id", world );
+          values.put( "stage_id", stage );
+          values.put( "score", 0 );
+          values.put( "rank", 0 );
+
+          db.insert( "STAGE", null, values );
+        }
+      }
+
+      // アイテム分
+      for ( int item_count = 0; item_count < item_value; item_count++ )
+      {
+        values.clear();
+        values.put( "item_id", item_count );
+        values.put( "number", 0 );
+
+        db.insert( "ITEM", null, values );
       }
 
 
@@ -161,7 +239,45 @@ public class UserDataManager
     }
   }
 
-  public void reset_save(){}
+  public void reset_save()
+  {
+    String sql;
+
+    try
+    {
+      // テーブルを削除
+      sql = "drop table CONFIG, STAGE, ITEM";
+      db.execSQL( sql );
+
+      // CONFIGテーブルを作成
+      sql = "create table CONFIG" +
+        "(name text not null," +
+        "value real not null," +
+        "primary key(name))";
+      db.execSQL( sql );
+
+      // STAGEテーブルを作成
+      sql = "create table STAGE" +
+        "(world_id integer not null," +
+        "stage_id integer not null," +
+        "score integer, not null" +
+        "rank integer, not null" +
+        "primary key(world_id, stage_id)";
+      db.execSQL( sql );
+
+      // ITEMテーブルを作成
+      sql = "create table ITEM" +
+        "(id integer not null," +
+        "number integer not null," +
+        "primary key(id))";
+      db.execSQL( sql );
+
+      insert_newdata();
+    } catch ( SQLException e )
+    {
+      Log.e( "reset_save()SQL ERROR", e.toString() );
+    }
+  }
 
 
 
