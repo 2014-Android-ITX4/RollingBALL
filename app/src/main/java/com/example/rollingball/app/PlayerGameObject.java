c85dfebe9f29897b07097c93a54ec28f9fc6bb29
Usagi Ito <usagi@WonderRabbitProject.net>
2014-06-11 15:53:08 +0900

    fix #115

--- app/src/main/java/com/example/rollingball/app/PlayerGameObject.java
+++ app/src/main/java/com/example/rollingball/app/PlayerGameObject.java
@@ -5,24 +5,25 @@ import java.util.LinkedList;
 
 public class PlayerGameObject extends RigidBodyGameObject implements IUpdatable
 {
-  PlayerGameObject( final InputManager s )
-  {}
   public LinkedList< Item > items;
   public InputManager input_manager;
+
+  PlayerGameObject( final InputManager s )
+  {}
 
   @Override
   public void update( final long delta_time_in_ns )
   {
-    // 傾きに応じて速度に力(F)を追加する
-    forces.add(input_manager.rotation);
+    // 傾きからかかる力を計算
+    // 傾きベクターの値の範囲 [ -π .. +π ] [rad]
+    // 力ベクターの値の範囲 [ -10 .. 10 ] [N] ( = [kg・m/(s・s)] )
+    // ハムスターの標準体重: 1.0e-1 [kg]
+    // 標準体重のハムスターを1[秒間]に1[m/(ss)]加速する程度の力f: 1.0e-1 [N]
+    final float max_force = 1.0e-1f;
 
-    Vec3 acceleration = new Vec3( 0.0f, 0.0f, 0.0f );
-    Vec3 force_sum    = new Vec3( 0.0f, 0.0f, 0.0f );
+    // 傾きに応じて速度に力(f)を追加する
+    forces.add( input_manager.rotation.multiply( max_force ) );
 
-    for( Vec3 f: forces )
-      force_sum = force_sum.add( f );
-
-    acceleration = force_sum.multiply( 1.0f / mass );
-    velocity = velocity.add( acceleration.multiply( delta_time_in_ns ) );
+    super.update( delta_time_in_ns );
   }
 }
