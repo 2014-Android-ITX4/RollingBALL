5296a4b9e2c828ec6457e5f4f9a0bc431c0f619d
aaaaaaai <arakawa85@gmail.com>
2014-07-02 13:29:52 +0900

    途中

--- app/src/main/java/com/example/rollingball/app/SceneCamera.java
+++ app/src/main/java/com/example/rollingball/app/SceneCamera.java
@@ -4,59 +4,54 @@ package com.example.rollingball.app;
  * Created by arakawa on 2014/06/11.
  */
 import android.content.Context;
-import android.graphics.Point;
 import android.view.View;
 import android.view.View.OnTouchListener;
-import android.view.Display;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
-import android.view.WindowManager;
 import android.util.Log;
+
+
+
 
 public class SceneCamera extends StageCamera implements OnTouchListener{
   private GestureDetector gestureDetector;
-
-  //Context con;
-
-  /*WindowManager wm = (WindowManager)con.getSystemService(Context.WINDOW_SERVICE);
-  private Display dis = wm.getDefaultDisplay();
-  int width = dis.
-*/
-
-  //動くのに必要
-  public boolean onTouch(View view, MotionEvent event)
-  {
-    return onTouchEvent( event );
-  }
 
   public SceneCamera (Context ctx)
   {
     gestureDetector = new GestureDetector( ctx, new GestureListener() );
   }
 
-  public boolean onTouchEvent(MotionEvent event) {
-    boolean flag = gestureDetector.onTouchEvent(event);
+  //動くのに必要
+  public boolean onTouch(View view, MotionEvent event)
+  {
+    Log.d( "app", "onTouch" );
+    return onTouchEvent( event );
+  }
 
-    /*if ( event.getX() > 0 && event.getX() <  && event.getY() >= 0 )
-    {
-      flag = false;
-    }*/
-    return flag;
+  public boolean onTouchEvent(MotionEvent event) {
+    Log.d("TouchEvent", "X:" + event.getX() + ",Y:" + event.getY());
+    return true;
   }
 
   private  class GestureListener extends SimpleOnGestureListener
+  //private SimpleOnGestureListener simpleOnGestureListener = new SimpleOnGestureListener()
   {
     private int swipe_distance = 100;
     private int swipe_velocity = 100;
+
+    @Override
+    public boolean onDown(MotionEvent e)
+    {
+      Log.d( "simple" , "Down");
+      return true;
+    }
 
     @Override
     public boolean onFling( MotionEvent e1, MotionEvent e2, float velocityX, float velocityY )
     {
       float diffX = e2.getX() - e1.getX();
       float diffY = e2.getY() - e1.getY();
-
-      Log.d( "MyApp", "Xの移動量" + diffX + "　Yの移動量" + diffY );
 
       if ( Math.abs( diffX ) > Math.abs( diffY ) && Math.abs( diffX ) > swipe_distance && Math.abs( velocityX ) > swipe_velocity )
       {
@@ -65,14 +60,14 @@ public class SceneCamera extends StageCamera implements OnTouchListener{
           //右にスワイプ
           //θ減少
           theta( theta() - Math.abs( diffX ) );
-          return true;
+          Log.d("simpleOn", "right");
         }
         else
         {
           //左にスワイプ
           //θ増加
           theta( theta() + Math.abs( diffX) );
-          return true;
+          Log.d("simpleOn", "left");
         }
       }
       else if ( Math.abs( diffY ) > Math.abs( diffX ) && Math.abs( diffY ) > swipe_distance && Math.abs( velocityY ) > swipe_velocity )
@@ -82,22 +77,22 @@ public class SceneCamera extends StageCamera implements OnTouchListener{
           //上にスワイプ
           //φ減少
           phi( phi() - Math.abs( diffY ) );
-          return true;
+          Log.d("simpleOn", "top");
         }
         else
         {
           //下にスワイプ
           //φ増加
           phi( phi() + Math.abs( diffY ) );
-          return true;
+          Log.d("simpleOn", "bottom");
         }
       }
       else
       {
         //45°斜めにスワイプ
         //フリック
-        return false;
       }
+      return false;
     }
   }
 }
