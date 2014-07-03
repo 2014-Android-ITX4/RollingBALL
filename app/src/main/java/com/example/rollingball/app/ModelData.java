dd9b17ae2ffacf57fec308dbaf5f11bd294d978a
Usagi Ito <usagi@WonderRabbitProject.net>
2014-07-04 08:26:49 +0900

    refactoring and reformat.

--- app/src/main/java/com/example/rollingball/app/ModelData.java
+++ app/src/main/java/com/example/rollingball/app/ModelData.java
@@ -18,19 +18,18 @@ public class ModelData
   private int _indices_buffer_id;
   private int _texture_buffer_id;
 
-  // TODO:テスト用なので後で消すなり変更するなりする
-  FloatBuffer _float_buffer;
-  ByteBuffer  _byte_buffer;
-
   public ModelData( float[] arg_vertices, byte[] arg_indices )
   {
-    make_float_VBO( arg_vertices );
-    make_byte_VBO( arg_indices );
+    _vertices_buffer_id = generate_vertex_buffer_from_float_buffer( create_float_buffer( arg_vertices ) );
+    //Log.d( "_vertices_buffer_id generated", String.valueOf( _vertices_buffer_id ) );
+
+    _indices_buffer_id = generate_index_buffer_from_byte_buffer( create_byte_buffer( arg_indices ) );
+    //Log.d( "_indices_buffer_id generated", String.valueOf( _indices_buffer_id ) );
   }
 
   public static ModelData generate_sphere( float radius )
   {
-    // TODO
+    // TODO: #151
     throw new NotImplementedException();
   }
 
@@ -63,44 +62,38 @@ public class ModelData
 
   public static ModelData load_from_file( String file_path )
   {
-    // TODO
+    // TODO: #152
     throw new NotImplementedException();
   }
 
   public void draw( final Mat4 transformation )
   {
+    // シェーダープログラムの取得
     IntBuffer program_id_buffer = IntBuffer.allocate( 1 );
     GLES20.glGetIntegerv( GLES20.GL_CURRENT_PROGRAM, program_id_buffer );
     int program_id = program_id_buffer.get();
 
-    int location_of_position = GLES20.glGetAttribLocation( program_id, "position" );
-
     // 頂点バッファの束縛
     GLES20.glBindBuffer( GLES20.GL_ARRAY_BUFFER, _vertices_buffer_id );
-    //GLES20.glEnableVertexAttribArray( location_of_position );
-    GLES20.glVertexAttribPointer( location_of_position, 4, GLES20.GL_FLOAT, false, 0, 0  );
-    //Log.d( "GL", "頂点バッファ指定 id:" + _vertices_buffer_id );
 
     // インデックスバッファの束縛
     GLES20.glBindBuffer( GLES20.GL_ELEMENT_ARRAY_BUFFER, _indices_buffer_id );
-    //Log.d( "GL", "インデックスバッファ指定 id:" + _indices_buffer_id );
+
+    // 頂点レイアウトの指定
+    GLES20.glVertexAttribPointer( GLES20.glGetAttribLocation( program_id, "position" ), 4, GLES20.GL_FLOAT, false, 0, 0  );
 
     // ワールド変換
-    int location_of_world_transformation = GLES20.glGetUniformLocation( program_id , "world_transformation" );
-    GLES20.glUniformMatrix4fv( location_of_world_transformation, 1, false, transformation.getBuffer() );
+    GLES20.glUniformMatrix4fv( GLES20.glGetUniformLocation( program_id , "world_transformation" ), 1, false, transformation.getBuffer( ) );
 
     // 拡散反射光の色
-    int location_of_diffuse = GLES20.glGetUniformLocation( program_id, "diffuse" );
-    GLES20.glUniform3fv( location_of_diffuse, 1, new Vec3(0.0f, 1.0f, 0.0f).getBuffer() );
+    GLES20.glUniform3fv( GLES20.glGetUniformLocation( program_id, "diffuse" ), 1, new Vec3( 0.0f, 1.0f, 0.0f ).getBuffer( ) );
 
     // 不透明度
-    int location_of_transparent = GLES20.glGetUniformLocation( program_id, "transparent" );
-    GLES20.glUniform1f( location_of_transparent, 1.0f );
+    GLES20.glUniform1f( GLES20.glGetUniformLocation( program_id, "transparent" ), 1.0f );
 
     // TODO: テクスチャー対応用。テクスチャーに対応する際にどうぞ
     // テクスチャーのブレンド比
-    //int location_of_diffuse_texture_blending_factor = GLES20.glGetUniformLocation( program_id, "location_of_diffuse_texture_blending_factor" );
-    //GLES20.glUniform1f( location_of_diffuse_texture_blending_factor, 0.0f );
+    //GLES20.glUniform1f( GLES20.glGetUniformLocation( program_id, "location_of_diffuse_texture_blending_factor" ), 0.0f );
 
     // 面群の描画
     GLES20.glDrawElements( GLES20.GL_TRIANGLE_STRIP, 10, GLES20.GL_UNSIGNED_BYTE, 0 );
@@ -114,46 +107,45 @@ public class ModelData
     //Log.d( "GL_NO_ERROR", String.valueOf( GLES20.glGetError() == GLES20.GL_NO_ERROR ) );
   }
 
-  // float[] --> int ( generated id of vertex buffer object )
-  private void make_float_VBO( float[] array )
+  // float[] --> FloatBuffer
+  private static FloatBuffer create_float_buffer( float[] float_array )
   {
-    _float_buffer = ByteBuffer.allocateDirect( array.length * 4 ).order( ByteOrder.nativeOrder()).asFloatBuffer();
-    _float_buffer.put( array ).position(0);
-
-    // VBO をGPUに生成し、CPUの FloatBuffer を GPU の VBO へ転送
-    int buffer_ids[] = new int[1];
-    GLES20.glGenBuffers(1,buffer_ids,0);
-    Log.d( "GL_NO_ERROR1", String.valueOf( GLES20.glGetError() == GLES20.GL_NO_ERROR ) );
-
-    GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, buffer_ids[0]);
-    Log.d( "GL_NO_ERROR2", String.valueOf( GLES20.glGetError() == GLES20.GL_NO_ERROR ) );
-
-    GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, _float_buffer.capacity() * 4, _float_buffer,GLES20.GL_STATIC_DRAW);
-    Log.d( "GL_NO_ERROR3", String.valueOf( GLES20.glGetError() == GLES20.GL_NO_ERROR ) );
-
-    _vertices_buffer_id = buffer_ids[0];
-
-    Log.d( "make_float_VBO buffer_id_end", String.valueOf( _vertices_buffer_id ) );
+    FloatBuffer b = ByteBuffer.allocateDirect( float_array.length * 4 ).order( ByteOrder.nativeOrder() ).asFloatBuffer( );
+    b.put( float_array ).position( 0 );
+    return b;
   }
 
-  // byte[] --> int ( generated id of vertex buffer object )
-  private void make_byte_VBO( byte[] array )
+  // FloatBuffer --> int ( generated id of vertex buffer object )
+  private static int generate_vertex_buffer_from_float_buffer( FloatBuffer b )
   {
-    _byte_buffer = ByteBuffer.allocateDirect(array.length).order( ByteOrder.nativeOrder());
-    _byte_buffer.put( array ).position(0);
+    // VBO をGPUに生成し、CPUの FloatBuffer を GPU の VBO へ転送
+    int buffer_ids[] = new int[ 1 ];
 
-    int[] buffer_ids=new int[1];
+    GLES20.glGenBuffers( 1, buffer_ids, 0 );
+    GLES20.glBindBuffer( GLES20.GL_ARRAY_BUFFER, buffer_ids[ 0 ] );
+    GLES20.glBufferData( GLES20.GL_ARRAY_BUFFER, b.capacity() * 4, b, GLES20.GL_STATIC_DRAW );
 
-    GLES20.glGenBuffers(1,buffer_ids,0);
-    Log.d( "GL_NO_ERROR4", String.valueOf( GLES20.glGetError() == GLES20.GL_NO_ERROR ) );
-    GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER,buffer_ids[0]);
-    Log.d( "GL_NO_ERROR5", String.valueOf( GLES20.glGetError() == GLES20.GL_NO_ERROR ) );
-    GLES20.glBufferData( GLES20.GL_ELEMENT_ARRAY_BUFFER, _byte_buffer.capacity(), _byte_buffer, GLES20.GL_STATIC_DRAW);
-    Log.d( "GL_NO_ERROR6", String.valueOf( GLES20.glGetError() == GLES20.GL_NO_ERROR ) );
+    return buffer_ids[ 0 ];
+  }
 
-    _indices_buffer_id = buffer_ids[0];
+  // byte[] --> ByteBuffer
+  private static ByteBuffer create_byte_buffer( byte[] byte_array )
+  {
+    ByteBuffer b = ByteBuffer.allocate( byte_array.length ).order( ByteOrder.nativeOrder( ) );
+    b.put( byte_array ).position( 0 );
+    return b;
+  }
 
-    Log.d( "make_byte_VBO buffer_id_end", String.valueOf( _indices_buffer_id ) );
+  // ByteBuffer --> int ( generated id of vertex buffer object )
+  private static int generate_index_buffer_from_byte_buffer( ByteBuffer b )
+  {
+    int[] buffer_ids=new int[ 1 ];
+
+    GLES20.glGenBuffers( 1, buffer_ids, 0 );
+    GLES20.glBindBuffer( GLES20.GL_ELEMENT_ARRAY_BUFFER,buffer_ids[ 0 ] );
+    GLES20.glBufferData( GLES20.GL_ELEMENT_ARRAY_BUFFER, b.capacity( ), b, GLES20.GL_STATIC_DRAW );
+
+    return buffer_ids[ 0 ];
   }
 
 }
