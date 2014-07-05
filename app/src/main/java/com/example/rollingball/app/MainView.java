19378a5f179a631b02df19099e09aa0b4b40c0d9
Usagi Ito <usagi@WonderRabbitProject.net>
2014-07-05 15:36:32 +0900

    rawリソースからStringを取得する機能を追加

--- app/src/main/java/com/example/rollingball/app/MainView.java
+++ app/src/main/java/com/example/rollingball/app/MainView.java
@@ -3,6 +3,11 @@ package com.example.rollingball.app;
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainView extends GLSurfaceView
 {
   public MainRenderer renderer;
@@ -22,6 +27,24 @@ public class MainView extends GLSurfaceView
     this.setRenderer( renderer );
 
     scene_manager = new SceneManager( this );
+  }
+
+  public String load_text_from_raw_resource( int resource_id )
+  {
+    InputStream s = getResources().openRawResource( resource_id );
+    InputStreamReader sr = new InputStreamReader( s );
+
+    final int buffer_size = 64 * 1024;
+
+    char[] char_buffer = new char[ buffer_size ];
+    int readed_size = 0;
+
+    try
+    { readed_size = sr.read( char_buffer, 0, buffer_size ); }
+    catch ( IOException e )
+    { e.printStackTrace(); }
+
+    return String.valueOf( char_buffer, 0, readed_size );
   }
 
 }
