7465ba809c82c68802ed62effc4c9220f3b244e4
Usagi Ito <usagi@WonderRabbitProject.net>
2014-07-09 12:09:10 +0900

    fix #204 Test1NPC 関連の初期の調整

--- app/src/main/java/com/example/rollingball/app/Test1NPC.java
+++ app/src/main/java/com/example/rollingball/app/Test1NPC.java
@@ -1,6 +1,10 @@
 package com.example.rollingball.app;
 
-public class Test1NPC
+public class Test1NPC extends NPCGameObject
 {
-  //public Test1NPCAI;
+  public Test1NPC( Scene scene )
+  {
+    super( scene );
+    this.ai( new Test1NPCAI( this ) );
+  }
 }
--- app/src/main/java/com/example/rollingball/app/Test1NPCAI.java
+++ app/src/main/java/com/example/rollingball/app/Test1NPCAI.java
@@ -1,6 +1,9 @@
 package com.example.rollingball.app;
 
-public class Test1NPCAI
+public class Test1NPCAI extends NPCAI
 {
-
+  public Test1NPCAI( final LifeGameObject control_target )
+  {
+    super( control_target );
+  }
 }
