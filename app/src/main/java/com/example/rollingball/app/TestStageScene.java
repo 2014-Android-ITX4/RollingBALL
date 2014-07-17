1ac78267b1059d793cd9404a02124a20e8234ebb
Usagi Ito <usagi@WonderRabbitProject.net>
2014-07-18 03:11:52 +0900

    テストステージを小型＆シンプルに

--- app/src/main/java/com/example/rollingball/app/TestStageScene.java
+++ app/src/main/java/com/example/rollingball/app/TestStageScene.java
@@ -11,15 +11,15 @@ public class TestStageScene extends StageScene
   {
     super( s );
 
-    field( new FieldGameObject( this, "test_1_64x64.png" ) );
+    field( new FieldGameObject( this, 16, 16 ) );
 
-    _player.position = new Vec3( _field.length_x() * 0.5f, 20.0f, _field.length_z() * 0.5f );
+    _player.position = new Vec3( _field.length_x() * 0.5f, 10.0f, _field.length_z() * 0.5f );
     _player.model = ModelData.generate_sphere();
     _player.model.material.diffuse_color( new Vec3( 0.6f, 0.6f, 1.0f ) );
     //game_objects.clear();
 
     // テスト用NPCを配置
-    random_npc( 32 );
+    random_npc( 3 );
 
     this.lighting.position( new Vec3( _player.position.getX(), _player.position.getY() + 25.0f, _player.position.getZ() ) );
   }
